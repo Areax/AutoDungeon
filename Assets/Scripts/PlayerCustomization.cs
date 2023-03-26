f1ba83cea1d587381969ae5bade26eba2e071164
Taylor Romero <taylor@Taylors-MacBook-Pro.local>
2023-03-26 14:54:56 -0700

    current character UI

---
+++ Assets/Scripts/PlayerCustomization.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCustomization : MonoBehaviour
+{
+    [Header("Sprite to Change")]
+    public SpriteRenderer bodyPart;
+
+    [Header("List of Options")]
+    public List<Sprite> options = new List<Sprite>();
+
+    private int currOption = 0;
+
+    public void nextOption(){
+        currOption++;
+        if(currOption>= options.Count){
+            currOption = 0;
+        }
+        bodyPart.sprite = options[currOption];
+    }
+    public void previousOption(){
+        currOption--;
+        if(currOption<=0){
+            currOption = options.Count -1;
+        }
+        bodyPart.sprite = options[currOption];
+    }
+}

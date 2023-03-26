ddaf73a02f7e2178c629b5f2ee31a9c99b4957d2
Brandon Eiler <eiler.brandon@gmail.com>
2023-03-26 16:29:48 -0700

    implemented stats struct onto enemy/player classes

--- Assets/Scripts/CharacterScripts/Enemy.cs
+++ Assets/Scripts/CharacterScripts/Enemy.cs
@@ -4,6 +4,55 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour, Character
 {
+
+    //  field for this enemies name
+    public string enemyName;
+
+    //  reference for this enemies stats struct
+    public EnemyStats enemyStats;
+
+    //  system.serializable allows this struct to be shown in the inspector
+    [System.Serializable]
+    public struct EnemyStats
+    {
+        //  fields for max / cur hp
+        public int maxHitPoints;
+        public int curHitPoints;
+        [Space]
+
+        // fields for max / cur speed
+        public int maxSpeed;
+        public int curSpeed;
+        [Space]
+
+        // fields for max / cur dex
+        public int maxDexterity;
+        public int curDexterity;
+        [Space]
+
+        // fields for max / cur strength
+        public int maxStrength;
+        public int curStrength;
+        [Space]
+
+        //   fields for max / cur wisdom
+        public int maxWisdom;
+        public int curWisdom;
+        [Space]
+
+        //  fields for max / cur intelligence
+        public int maxIntelligence;
+        public int curIntelligence;
+        [Space]
+
+        //  fields for the max / cur charisma
+        public int maxCharisma;
+        public int curCharisma;
+    }
+
+    //  field for the level of this unit
+    public int level;
+
     public EnemyData enemyData;
     public int GetLevel()
     {
@@ -18,5 +67,22 @@ public class Enemy : MonoBehaviour, Character
     public Stats GetStats()
     {
         return enemyData.enemyStats;
+    }
+
+
+    //  for debugging enemy death--
+    public void Awake()
+    {
+        
+    }
+
+    public void OnMouseEnter()
+    {
+        Debug.Log("mouse entered " + this.GetName() + " collider");
+    }
+
+    public void OnMouseExit()
+    {
+        Debug.Log("mouse exited " + this.GetName() + " collider");
     }
 }
